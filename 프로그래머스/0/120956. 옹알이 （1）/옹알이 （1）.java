ffa91ced512d7857579b12c25e8d06cac9a04789
sychoi <77200640+FutureDevSY@users.noreply.github.com>
2024-10-16 02:16:50 +0900

    [level 0] Title: 옹알이 (1), Time: 1.42 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String str;
+        int idx;
+        String[] canSpeak = {"aya", "ye", "woo", "ma"};
+
+        for(int i=0; i<babbling.length; i++){
+            str = babbling[i];
+            
+            for(int j=0; j<canSpeak.length; j++){
+                idx = str.indexOf(canSpeak[j]);
+                if(idx != -1) {
+                    // System.out.println(canSpeak[j] + " " + str);
+                    str = str.replace(canSpeak[j], " ");
+                    
+                }
+            }
+            for(int j=0; j<canSpeak.length; j++){
+                str = str.replace(" ", "");
+            }     
+            
+            if(str.length() == 0) answer++;
+        }
+        
+        return answer;
+    }
+}
