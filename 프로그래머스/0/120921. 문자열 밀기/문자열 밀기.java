a5721bea56cbdd4ed2f9a2b768df984559a7e4c5
sychoi <77200640+FutureDevSY@users.noreply.github.com>
2024-10-16 01:16:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.13 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,26 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = -1;
-        int strLng = A.length();
-        int idx;
-        StringBuilder sb;
-
-        if(A.equals(B)) return 0;
-
-        for(int i=strLng - 1; i >= 0; i--){
-            sb = new StringBuilder();
-            for(int j=i; j < i + strLng; j++){
-                idx = j;
-                if(j > strLng - 1) idx = j - strLng;
-                sb.append(A.charAt(idx));
-            }
-            // System.out.println(sb.toString());
-            if(B.equals(sb.toString())) {
-                answer = strLng - i;
-                break;
-            }
-        }
-        
+        int answer = 0;
         return answer;
     }
 }
