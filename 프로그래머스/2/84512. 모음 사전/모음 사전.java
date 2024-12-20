ffb64ac595bbec24e38aacbb8a25bb386b314c2c
sychoi <77200640+FutureDevSY@users.noreply.github.com>
2024-12-20 16:37:08 +0900

    [level 2] Title: 모음 사전, Time: 77.61 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,36 @@
+class Solution {
+    static boolean found;
+    static int resultOrder;
+    static char[] group = {'A', 'E', 'I', 'O', 'U'};
+    
+    public int solution(String word) {
+        return recur(word, new StringBuilder(), word.length());
+    }
+    
+    static int ord;
+    
+    public int recur(String word, StringBuilder sb, int wordLng){
+        System.out.println(sb.toString() + " " + ord);
+        if(wordLng == sb.length()){
+            if(sb.toString().equals(word)){
+                return ord;
+            }
+        }
+        
+        int tmp;
+        if(sb.length() < 5){
+            for(int i=0; i<5; i++){
+                ord++;
+                tmp = recur(word, sb.append(group[i]), wordLng);
+                if (tmp != -1){
+                    return ord;
+                }else if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }    
+        }
+        
+        return -1;
+        
+    }
+}
